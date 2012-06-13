68ca1949b258eb373cf70e7cc310dc4de44f04ca
Marco <markolino-leggends@live.it>
2012-06-13 15:43:18 +0300

    Update master

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,7 +10,7 @@ TARGET_KERNEL_CONFIG := cyanogenmod_p506_defconfig
 TARGET_BOOTLOADER_BOARD_NAME := p506
 TARGET_OTA_ASSERT_DEVICE := thunderg,p506
 
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p505 no_console_suspend
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p506 no_console_suspend
                         
 USE_CAMERA_STUB := false
 
