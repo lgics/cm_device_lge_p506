4e6b9127824905640a57b431ebb8f7147e22521f
Arjen Visser <arjen.jessica@gmail.com>
2012-07-21 19:41:21 +0300

    update BOARD_KERNEL_CMDLINE

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,7 +2,7 @@
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
 TARGET_KERNEL_CONFIG := cyanogenmod_p505_defconfig
-BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p505 no_console_suspend
+BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p506 no_console_suspend
 
 TARGET_BOOTLOADER_BOARD_NAME := p506
 TARGET_OTA_ASSERT_DEVICE := thunderg,p506
