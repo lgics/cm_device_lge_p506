b31ae2ee1ae53af630a9c477a635c7d24508e0f5
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-09-30 18:16:32 -0400

    Revert "Use ICS libaudio blob"
    This reverts commit d1d4ae1f55e5a0ed422a5bc8196b7e7037ebd71d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 -include vendor/lge/p506/BoardConfigVendor.mk
 -include device/lge/msm7x27-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_p505_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_p506_defconfig
 BOARD_KERNEL_CMDLINE := mem=471M console=ttyMSM2,115200n8 androidboot.hardware=p506 no_console_suspend
 
 TARGET_BOOTLOADER_BOARD_NAME := p506
@@ -9,8 +9,6 @@ TARGET_OTA_ASSERT_DEVICE := thunderg,p506
 
 USE_CAMERA_STUB := false
 
-TARGET_PROVIDES_LIBAUDIO := true
-COMMON_GLOBAL_CFLAGS += -DICS_AUDIO_BLOB
 # Enable ICS-backwards compatibility    
 COMMON_GLOBAL_CFLAGS += -DQCOM_ICS_COMPAT
 # Disable PIE since it breaks ICS camera blobs
